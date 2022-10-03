b92424d78a316e1709aa4a86cf6c90881d6915cc
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2022-10-03 15:18:44 -0700

    Update phylosem2fitted_DAG.R

--- R/phylosem2fitted_DAG.R
+++ R/phylosem2fitted_DAG.R
@@ -10,10 +10,10 @@ function( x ){
 
   # extract and name identical to output from fitted_DAG
   out = list(
-    coef = t(psem$report$Rho_jj),
-    se = t(as.list(psem$opt$SD,what="Std. Error",report=TRUE)$Rho_jj)
+    coef = t(x$report$Rho_jj),
+    se = t(as.list(x$opt$SD,what="Std. Error",report=TRUE)$Rho_jj)
   )
-  dimnames(out$coef) = dimnames(out$coef) = list( colnames(psem$data), colnames(psem$data) )
+  dimnames(out$coef) = dimnames(out$coef) = list( colnames(x$data), colnames(x$data) )
 
   # pass out
   class(out) = "fitted_DAG"
