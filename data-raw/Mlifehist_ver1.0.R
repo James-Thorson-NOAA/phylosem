039a8a77fd479dc8324330e551c9069271493b4c
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2023-09-03 18:12:27 -0700

    fixing notes in CRAN winbuild

--- data-raw/Mlifehist_ver1.0.R
+++ data-raw/Mlifehist_ver1.0.R
@@ -3,7 +3,7 @@ data_dir =  R'(C:\Users\James.Thorson\Desktop\Git\phylosem\data-raw)'
 
 # Load and format
 Mlifehist_ver1_0 = read.csv( file.path(data_dir,"Mlifehist_ver1.0.csv") )
-Mlifehist_ver1_0 = Mlifehist_ver1_0[,-match(c("GrowthRef","Mref","tmaxRef"),names(Mlifehist_ver1_0))]
+Mlifehist_ver1_0 = Mlifehist_ver1_0[,-match(c("GrowthRef","Mref","tmaxRef","SpecificLocation","CommonName"),names(Mlifehist_ver1_0))]
 
 # Export
 setwd( R'(C:\Users\James.Thorson\Desktop\Git\phylosem)' )
