8bcf4a784afa0e4372e177d68c2e517d746bc40c
James T. Thorson <James.T.Thorson@gmail.com>
2026-08-11 08:51:20 +0200

    Update test-family.R

--- tests/testthat/test-family.R
+++ tests/testthat/test-family.R
@@ -1,9 +1,8 @@
 
 context("Testing cross platform and R version compatibility")
 
-# Eastern Bering Sea pollcok
 test_that("phylosem Poisson-PCGLMM is working ", {
-  # Settings
+  skip_on_cran()
   Ntree = 100
   sd_x = 0.3
   sd_y = 0.3
@@ -30,7 +29,6 @@ test_that("phylosem Poisson-PCGLMM is working ", {
     family = "poisson",
     data = Data,
     phy = tree )
-  knitr::kable(summary(pglmm), digits=3)
 
   #
   pgsem = phylosem(
@@ -46,5 +44,49 @@ test_that("phylosem Poisson-PCGLMM is working ", {
     as.numeric(subset(summary(pgsem)$coefficients, Path == "x -> y" )['Estimate']),
     as.numeric(pglmm$B['x',]),
     tol = 1e-2
+  )
+})
+
+test_that("phylosem Binomial-PCGLMM is working ", {
+  skip_on_cran()
+  Ntree = 100
+  sd_x = 0.3
+  sd_y = 0.3
+  b0_x = 1
+  b0_y = 0
+  b_xy = 1
+
+  # Simulate tree
+  set.seed(1)
+  tree = ape::rtree(n=Ntree)
+
+  # Simulate data
+  x = b0_x + sd_x * phylolm::rTrait(n = 1, phy=tree)
+  ybar = b0_y + b_xy*x
+  y_normal = ybar + sd_y * phylolm::rTrait(n = 1, phy=tree)
+  y_binom = rbinom( n=Ntree, size=1, prob=plogis(y_normal) )
+
+  # Construct, re-order, and reduce data
+  Data = data.frame(x=x,y=y_binom)
+
+  #
+  pglmm = phyr::pglmm_compare(
+    y ~ 1 + x,
+    family = "binomial",
+    data = Data,
+    phy = tree )
+
+  #
+  pgsem = phylosem( sem = "x -> y, p",
+            data = Data,
+            family = list( x = fixed(), y = binomial("logit") ),
+            tree = tree,
+            control = phylosem_control(quiet = TRUE) )
+
+              #
+  expect_equal(
+    as.numeric(subset(summary(pgsem)$coefficients, Path == "x -> y" )['Estimate']),
+    as.numeric(pglmm$B['x',]),
+    tol = 5e-2
   )
 })
